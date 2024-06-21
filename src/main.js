bb717ec984ecd01e09f400a6c017fa5566cfe57e
unknown <gervasiob@hotmail.com>
2024-06-21 13:34:01 -0300

    feat: ajustes

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import { router } from '@/router';
-
+import ZoomOnHover from "vue-zoom-on-hover";
 
 const app = createApp(App);
+
+app.use(ZoomOnHover);
 app.use(router);
 app.use(Antd).mount('#app');
