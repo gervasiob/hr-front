36ee9ca9c765ef285764d4b2e88fad6b84f4dfda
gervasiob <32315669+gervasiob@users.noreply.github.com>
2024-07-24 12:48:39 -0300

    Update stocksIndex.vue

--- src/views/stocks/stocksIndex.vue
+++ src/views/stocks/stocksIndex.vue
@@ -16,7 +16,7 @@
       
 
 
-        <a-col :span="8" style="text-align: right">
+        <a-col :span="6" :offset="6" style="text-align: right">
           <a-button type="primary" danger @click="onSearch">Buscar</a-button>
           <a-button style="margin: 0 8px" @click="() => resetFilters()">Borrar Filtros</a-button>
         </a-col>
