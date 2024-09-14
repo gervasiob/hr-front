b8db4252b7120d078d9ca89cdc07c2cd895ee75f
unknown <gervasiob@hotmail.com>
2024-09-14 11:14:50 -0300

    feat: boton buscar costo con llanta

--- src/api/costs/costs.js
+++ src/api/costs/costs.js
@@ -8,6 +8,9 @@ export async function getCosts(params, id = null) {
 export async function getTireCost(params) {
     return await apiRequest('post', 'tire-cost/', params);
 }
+export async function getLlantaCost(params) {
+    return await apiRequest('post', 'llanta-cost/', params);
+}
 
 export async function addCosts(params) {
     return await apiRequest('post', ENDPOINT, params);
@@ -16,12 +19,18 @@ export async function addCosts(params) {
 export async function updateCosts(id, params) {
     return await apiRequest('put', ENDPOINT, params, id);
 }
-export async function upload( params) {
-    return await apiRequest('put', ENDPOINT, params, id);
+export async function upload(params) {
+    return await apiRequest('put', ENDPOINT, params);
 }
 
 export async function deleteCosts(id, params) {
     return await apiRequest('delete', ENDPOINT, params, id);
+}
+export async function getDescriptionList(params) {
+    return await apiRequest('get', 'costs_description_deep/', params);
+}
+export async function getSkuList(params) {
+    return await apiRequest('get', 'costs_sku_deep/', params);
 }
 
 
