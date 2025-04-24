f0b5aec86a1f08e3a20f03cb404b40da6b26899c
unknown <gervasiob@hotmail.com>
2025-04-23 21:23:21 -0300

    vuelta a prod

--- src/api/apiUrls.js
+++ src/api/apiUrls.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // export const BASE_URL = 'http://192.168.0.206:8001/';
 import { useRoute } from 'vue-router';
 
-const stage = 'DEV'; // Cambiar por PROD
+const stage = 'PROD'; // Cambiar por PROD
 localStorage.setItem('origin', window.location.origin)
 localStorage.setItem('stage', stage)
 // export const BASE_URL = 'https://dft-back-uat-b85d882277cf.herokuapp.com/';
